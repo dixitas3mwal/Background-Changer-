5d5276e42ff54a7625505bce6a8753bcd96bf955
dixi <deekshitasemwal@gmail.com>
2026-06-18 12:24:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ document.body.style.backgroundColor = color;
 ```
 
 }
+
+function randomColor(){
+
+    let colors = [
+        "red",
+        "blue",
+        "green",
+        "purple",
+        "orange",
+        "pink"
+    ];
+
+    let random =
+    Math.floor(Math.random() * colors.length);
+
+    document.body.style.backgroundColor =
+    colors[random];
+}
