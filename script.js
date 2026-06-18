cab1f4f4aefb063b248a6ae1db7a5b3081a763b1
dixi <deekshitasemwal@gmail.com>
2026-06-18 12:42:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,20 +6,3 @@ document.body.style.backgroundColor = color;
 
 }
 
-function randomColor(){
-
-    let colors = [
-        "red",
-        "blue",
-        "green",
-        "purple",
-        "orange",
-        "pink"
-    ];
-
-    let random =
-    Math.floor(Math.random() * colors.length);
-
-    document.body.style.backgroundColor =
-    colors[random];
-}
